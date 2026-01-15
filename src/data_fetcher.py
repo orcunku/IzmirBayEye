fe479a1898d3520788413fa6b9b44db8fbb0e8f6
orcunku <orcunkusdemirrr@gmail.com>
2026-01-15 09:23:41 +0000

    Refactor get_weather_data to get_environmental_data
    Updated the method to fetch environmental data, including water temperature and chlorophyll levels.

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -1,15 +1,9 @@
 import random
 
 class DataFetcher:
-    """Simulates fetching real-time environmental data for Izmir Bay."""
-    def get_weather_data(self):
+    def get_environmental_data(self):
         return {
-            "wind_speed": random.uniform(2, 12), 
-            "air_temp": random.uniform(22, 35)
-        }
-
-    def get_satellite_indices(self):
-        return {
-            "chlorophyll_index": random.uniform(0.1, 0.9),
-            "turbidity": random.uniform(10, 80)
+            "wind_speed": random.uniform(2, 15),
+            "water_temp": random.uniform(20, 35),
+            "chlorophyll": random.uniform(0.1, 0.9)
         }
